982bbf809db90cd467043008fd105e2d328a17e2
TimGaneev <tim.ganeev718@gmail.com>
2025-12-22 21:53:51 +0400

    UI теперь сохраняет пропорции изображений

--- Lab 5/turtle_app.py
+++ Lab 5/turtle_app.py
@@ -1,6 +1,6 @@
 import img_iterator as imit
 import main_window
-from PyQt6 import QtWidgets, QtGui
+from PyQt6 import QtWidgets, QtGui, QtCore
 
 
 class TurtleApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
@@ -8,12 +8,18 @@ class TurtleApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.iterator = imit.PathsIterator(csv_path)
-        self.image.setPixmap(QtGui.QPixmap(self.iterator.cur()))
+        image = QtGui.QPixmap(self.iterator.cur())
+        image = image.scaled(600, 400, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        self.image.setPixmap(image)
         self.nextimage.clicked.connect(self.next_image)
         self.previmage.clicked.connect(self.prev_image)
 
     def next_image(self):
-        self.image.setPixmap(QtGui.QPixmap(self.iterator.__next__()))
+        image = QtGui.QPixmap(self.iterator.__next__())
+        image = image.scaled(600, 400, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        self.image.setPixmap(image)
 
     def prev_image(self):
-        self.image.setPixmap(QtGui.QPixmap(self.iterator.prev()))
+        image = QtGui.QPixmap(self.iterator.prev())
+        image = image.scaled(600, 400, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        self.image.setPixmap(image)
